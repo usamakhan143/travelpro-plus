dcfc4725f64c8ca9a22029529d956ad740a8ace9
Usama Khan <usamaoff796@gmail.com>
2024-09-27 02:45:24 +0500

    Done working on adding icons in search form fields and also enhance the UI of hotel card.

--- includes/templates/hotels/hotel-search.php
+++ includes/templates/hotels/hotel-search.php
@@ -11,6 +11,7 @@
                         <div class="col-md-3 p-0">
                             <div class="travelpro-search-flight-input-group-2">
                                 <label class="bootstrap-form-label">Destination</label>
+                                <i class="fa-solid fa-location-dot"></i>
                                 <input class="form-control input--style-1" type="text" id="travelpro-plus-hotel-destination" name="hotel-destination" placeholder="City, Region" required="required">
                                 <div class="icon-container hotel-destination-loader">
                                     <i class="spinner"></i>
@@ -26,19 +27,27 @@
                             <input class="form-control input--style-1" type="date" name="hotel-check-out" required id="end-date">
                         </div> -->
                         <div class="col-md-2 field-padding">
-                            <label class="bootstrap-form-label">Check-in</label>
-                            <input class="form-control input--style-1" type="text" name="hotel-check-in" required id="flat-start-date" placeholder="Check-in Date" readonly>
+                            <div class="travelpro-search-flight-input-group-2">
+                                <label class="bootstrap-form-label">Check-in</label>
+                                <i class="fa-regular fa-calendar-days"></i>
+                                <input class="form-control input--style-1" type="text" name="hotel-check-in" required id="flat-start-date" placeholder="Check-in Date" readonly>
+                            </div>
                         </div>
                         <div class="col-md-2 field-padding">
-                            <label class="bootstrap-form-label">Check-out</label>
-                            <input class="form-control input--style-1" type="text" name="hotel-check-out" required id="flat-end-date" placeholder="Check-Out Date" readonly>
+                            <div class="travelpro-search-flight-input-group-2">
+                                <label class="bootstrap-form-label">Check-out</label>
+                                <i class="fa-regular fa-calendar-days"></i>
+                                <input class="form-control input--style-1" type="text" name="hotel-check-out" required id="flat-end-date" placeholder="Check-Out Date" readonly>
+                            </div>
                         </div>
                         <div class="col-md-3 field-padding people-selector-field">
-                            <label class="bootstrap-form-label">Adult & Children</label>
-                            <input class="form-control input--style-1" type="text" id="peopleInput" name="peoples" placeholder="Select Adults, Child" readonly>
-                            <div id="peopleDropdown" class="dropdown-content">
-                                <!-- Rooms Selector -->
-                                <!-- <label for="rooms">Rooms:</label>
+                            <div class="travelpro-search-flight-input-group-2">
+                                <label class="bootstrap-form-label">Adult & Children</label>
+                                <i class="fa-solid fa-users"></i>
+                                <input class="form-control input--style-1" type="text" id="peopleInput" name="peoples" placeholder="Select Adults, Child" readonly>
+                                <div id="peopleDropdown" class="dropdown-content">
+                                    <!-- Rooms Selector -->
+                                    <!-- <label for="rooms">Rooms:</label>
                                 <select id="rooms">
                                     <option value="1">1 Room</option>
                                     <option value="2">2 Rooms</option>
@@ -47,32 +56,39 @@
                                     <option value="5">5 Rooms</option>
                                 </select> -->
 
-                                <!-- Adults Selector -->
-                                <label for="numberOfAdultsInHotel">Adults:</label>
-                                <select id="numberOfAdultsInHotel">
-                                    <option value="1">1 Adult</option>
-                                    <option value="2">2 Adults</option>
-                                    <option value="3">3 Adults</option>
-                                    <option value="4">4 Adults</option>
-                                    <option value="5">5 Adults</option>
-                                </select>
+                                    <!-- Adults Selector -->
+                                    <div class="combo-field-container">
+                                        <label for="numberOfAdultsInHotel">Adults:</label>
+                                        <i class="fa-solid fa-users"></i>
+                                        <select id="numberOfAdultsInHotel" required>
+                                            <option value="1">1 Adult</option>
+                                            <option value="2">2 Adults</option>
+                                            <option value="3">3 Adults</option>
+                                            <option value="4">4 Adults</option>
+                                            <option value="5">5 Adults</option>
+                                        </select>
+                                    </div>
 
-                                <!-- Children Selector -->
-                                <label for="children">Children:</label>
-                                <select id="children">
-                                    <option value="0">0 Children</option>
-                                    <option value="1">1 Child</option>
-                                    <option value="2">2 Children</option>
-                                    <option value="3">3 Children</option>
-                                    <option value="4">4 Children</option>
-                                    <option value="5">5 Children</option>
-                                </select>
+                                    <!-- Children Selector -->
+                                    <div class="combo-field-container">
+                                        <label for="children">Children:</label>
+                                        <i class="fa-solid fa-children"></i>
+                                        <select id="children">
+                                            <option value="0">0 Children</option>
+                                            <option value="1">1 Child</option>
+                                            <option value="2">2 Children</option>
+                                            <option value="3">3 Children</option>
+                                            <option value="4">4 Children</option>
+                                            <option value="5">5 Children</option>
+                                        </select>
+                                    </div>
 
-                                <!-- Child Age Dropdowns Container -->
-                                <div id="childAgesContainer"></div>
+                                    <!-- Child Age Dropdowns Container -->
+                                    <div id="childAgesContainer"></div>
 
-                                <!-- Confirm Selection Button -->
-                                <button id="confirmSelection">Confirm Selection</button>
+                                    <!-- Confirm Selection Button -->
+                                    <button id="confirmSelection">Confirm Selection</button>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-2 field-padding">
