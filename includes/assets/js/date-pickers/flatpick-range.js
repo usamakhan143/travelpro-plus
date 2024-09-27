95ef05decaf396c2727ae61e2768d76a3b3af546
Usama Khan <usamaoff796@gmail.com>
2024-09-27 05:26:20 +0500

    Fixes the position of autocomplete field loader. restricte the user to not to select the same dates in search form.

--- includes/assets/js/date-pickers/flatpick-range.js
+++ includes/assets/js/date-pickers/flatpick-range.js
@@ -8,12 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
     minDate: "today", // Disable all dates before today
     onChange: function (selectedDates, dateStr, instance) {
       if (selectedDates.length === 2) {
-        // Range selected
-        const startDate = selectedDates[0]; // First selected date
-        const endDate = selectedDates[1]; // Second selected date
+        const startDate = selectedDates[0]; // First selected date (start date)
+        const endDate = selectedDates[1]; // Second selected date (end date)
 
-        startDateInput.value = instance.formatDate(startDate, "Y-m-d");
-        endDateInput.value = instance.formatDate(endDate, "Y-m-d");
+        if (startDate.getTime() === endDate.getTime()) {
+          // Reset the second date if start and end date are the same
+          instance.clear(); // Clear both dates
+        } else {
+          // Set start and end date values
+          startDateInput.value = instance.formatDate(startDate, "Y-m-d");
+          endDateInput.value = instance.formatDate(endDate, "Y-m-d");
+        }
       }
     },
   });
